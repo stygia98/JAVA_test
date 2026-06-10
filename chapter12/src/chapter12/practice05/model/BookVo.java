1933c1126ec71b3ad3c88d06b0cebf72e40836d3
stygia98 <stygia98@gmail.com>
2026-06-10 12:44:59 +0900

    [2026-06-10] [practice] 도서정보관리 수정

--- chapter12/src/chapter12/practice05/model/BookVo.java
+++ chapter12/src/chapter12/practice05/model/BookVo.java
@@ -1,5 +1,7 @@
 package chapter12.practice05.model;
 
+import java.util.Objects;
+
 public class BookVo {
 	private String title;
 	private String genre;
@@ -23,6 +25,22 @@ public class BookVo {
 	public int getRentCount() {return rentCount;}
 	public void setRentCount(int rentCount) {this.rentCount = rentCount;}
 	
+	@Override
+	public int hashCode() {
+		return Objects.hash(genre, title);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if(!(obj instanceof BookVo)) {
+			return false;
+		}
+		BookVo bV = (BookVo)obj;
+		if(this.title.equals(bV.title) && this.genre.equals(bV.genre))
+			return true;
+		return false;
+	}
+
 	public String inform() {
 		return "[도서명:" + title + " / 장르:" + genre + " / 대여 횟수:" + rentCount + "]";
 	}
